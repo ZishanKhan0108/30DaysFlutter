8af5dfbf3f34a50870eb636285892bc4e17d2fc7
ZishanKhan0108 <zishankhan003@gmail.com>
2025-07-06 17:46:35 +0530

    day8 complete

---
+++ lib/models/catalog.dart
@@ -0,0 +1,10 @@
+class items{
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  items({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+}
