fd196c8d3405f08ea4ce7f779b2652b893bcf942
ZishanKhan0108 <zishankhan003@gmail.com>
2025-07-06 16:32:46 +0530

    day6 completed

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepPurple,
         fontFamily: GoogleFonts.lato().fontFamily,
       ),
-      debugShowCheckedModeBanner: false,
+      // debugShowCheckedModeBanner: false,
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
